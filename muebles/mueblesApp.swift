0c2c846fb79e3ba5816d635fec8465fca2e7b44c
MuhammadZaidShahid <138578016+MuhammadZaidShahid@users.noreply.github.com>
2024-02-07 04:02:39 +0500

    Initial Commit

---
+++ muebles/mueblesApp.swift
@@ -0,0 +1,17 @@
+//
+//  mueblesApp.swift
+//  muebles
+//
+//  Created by Zaid's Mac on 07/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct mueblesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
